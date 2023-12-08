8cbf89204beb041aea108633fda41cd26f122a80
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2023-12-08 02:23:48 +0000

    squareroot calc & squareroot results is working

--- app.rb
+++ app.rb
@@ -2,8 +2,27 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_calc)
+end
+get("/square/new") do
+  erb(:square_calc)
+end
+get("/square/results") do
+
+@the_num = params.fetch("users_number").to_f
+
+@the_results = @the_num **2
+erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_calc)
+end
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_results = Math.sqrt(@the_num)
+  
+  erb(:square_root_results)
+  
 end
