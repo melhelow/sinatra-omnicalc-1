7461e8e4707ccc35d64fe71f457a5f18aaf71a9d
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2023-12-08 23:07:03 +0000

    removed the fuction , replaced it by variable ,fixed the result accuracy , added the percentage and the curruncy

--- app.rb
+++ app.rb
@@ -26,3 +26,28 @@ get("/square_root/results") do
   erb(:square_root_results)
   
 end
+get("/payment/new") do
+  erb(:payment_calc)
+end
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @years = params.fetch("user_years").to_i
+  @principal = params.fetch("user_pay").to_f
+
+  rate = @apr
+  bal = @principal
+  term = @years * 12
+
+  r = rate / 1200
+  n = r * bal
+  d = 1 - (1 + r)**-term
+  pmt = n / d
+
+
+  @pmayt = pmt.to_fs(:currency)
+  @apr = rate.to_fs(:percentage, { :precision => 4 } )
+  @principal = bal.to_fs(:currency)
+
+
+  erb(:payment_results)
+end
